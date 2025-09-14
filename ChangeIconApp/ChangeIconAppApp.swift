e31ca67eb8ba979b5c15d39b3e8651cde36d3f63
Javier Calatrava <javi.calatrava@gmail.com>
2025-09-14 10:43:17 +0200

    Blank project

---
+++ ChangeIconApp/ChangeIconAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeIconAppApp.swift
+//  ChangeIconApp
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 14/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ChangeIconAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
